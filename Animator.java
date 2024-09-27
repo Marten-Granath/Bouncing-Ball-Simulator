7e93a3016397c7c130d9868dad3e2c3dab6f7427
Marten-Granath <115485279+Marten-Granath@users.noreply.github.com>
2024-09-27 07:35:00 +0200

    Bouncing Balls Basics

--- Animator.java
+++ Animator.java
@@ -8,10 +8,13 @@ import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.geom.Ellipse2D;
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 import java.util.Random;
+import java.io.File;
+import java.io.IOException;
 
 import bouncing_balls.Model.Ball;
 
@@ -24,6 +27,7 @@ import bouncing_balls.Model.Ball;
 @SuppressWarnings("serial")
 public final class Animator extends JPanel implements ActionListener {
 
+	private Image backgroundImage;
 	public Animator(int pixelWidth, int pixelHeight, int fps) {
 		super(true);
 		this.timer = new Timer(1000 / fps, this);
@@ -31,6 +35,13 @@ public final class Animator extends JPanel implements ActionListener {
 		this.model = new Model(pixelWidth / pixelsPerMeter, pixelHeight / pixelsPerMeter);
 		this.setOpaque(false);
 		this.setPreferredSize(new Dimension(pixelWidth, pixelHeight));
+
+		// Load background image
+		try {
+			backgroundImage = ImageIO.read(new File("radams.png")); // Update the path to your image
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	/**
@@ -53,64 +64,66 @@ public final class Animator extends JPanel implements ActionListener {
 	 */
 	private double deltaT;
 
-	//private Random random = new Random();
-
 	public void start() {
 		timer.start();
 	}
 
 	public void stop() {
-    	timer.stop();
-    }
+		timer.stop();
+	}
 
 	@Override
 	protected void paintComponent(Graphics g) {
 		Graphics2D g2 = (Graphics2D) g;
-		// clear the canvas
-		g2.setColor(Color.BLACK);
-		g2.fillRect(0, 0, this.getWidth(), this.getHeight());
+
+		// Draw the background image if the flag is set
+		if (model.showBackgroundImage) {
+			g2.drawImage(backgroundImage, 0, 0, this);
+		} else {
+			// Clear the canvas
+			g2.setColor(Color.BLACK);
+			g2.fillRect(0, 0, this.getWidth(), this.getHeight());
+		}
 
 		// draw balls
-		//g2.setColor(Color.YELLOW);
 		for (Ball b : model.balls) {
 			double x = b.x - b.radius;
 			double y = b.y + b.radius;
 
+			// Draw the ball with a black fill
 			g2.setColor(Color.BLACK);
-			// paint balls (y-coordinates are inverted)
 			Ellipse2D.Double e = new Ellipse2D.Double(x * pixelsPerMeter, this.getHeight() - (y * pixelsPerMeter),
 					b.radius * 2 * pixelsPerMeter, b.radius * 2 * pixelsPerMeter);
 			g2.fill(e);
 
-			//g2.setColor(neonColors[randomIndex]);
+			// Draw only the outlines of the balls
 			g2.setColor(b.color);
 			g2.setStroke(new BasicStroke(5));
-
 			g2.draw(e);
 		}
 		Toolkit.getDefaultToolkit().sync();
 	}
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-    	model.step(deltaT);
-    	this.repaint();
-    }
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		model.step(deltaT);
+		this.repaint();
+	}
 
 	public static void main(String[] args) {
-        // Schedule a job for the event-dispatching thread:
-        // creating and showing this application's GUI.
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                Animator anim = new Animator(800, 600, 60);
-                JFrame frame = new JFrame("Bouncing balls");
-            	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            	frame.add(anim);
-            	frame.pack();
-            	frame.setLocationRelativeTo(null);
-            	frame.setVisible(true);
-            	anim.start();
-            }
-        });
-    }
+		// Schedule a job for the event-dispatching thread:
+		// creating and showing this application's GUI.
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				Animator anim = new Animator(800, 600, 60);
+				JFrame frame = new JFrame("Bouncing balls");
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.add(anim);
+				frame.pack();
+				frame.setLocationRelativeTo(null);
+				frame.setVisible(true);
+				anim.start();
+			}
+		});
+	}
 }
--- Model.java
+++ Model.java
@@ -1,5 +1,6 @@
 package bouncing_balls;
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Random;
 
@@ -22,6 +23,8 @@ class Model {
 	Ball [] balls;
 	Random random = new Random();
 
+
+	//Array of neon colors
 	private Color[] neonColors = {
 			new Color(57, 255, 20),   // Neon green
 			new Color(255, 20, 147),  // Neon pink
@@ -36,18 +39,8 @@ class Model {
 			new Color(125, 249, 255), // Electric blue
 			new Color(255, 69, 0)     // Neon orange-red
 	};
-	Model(double width, double height) {
-		areaWidth = width;
-		areaHeight = height;
-		
-		// Initialize the model with a few balls
-		balls = new Ball[2];
-
-		balls[0] = new Ball(0.5, 2, 2, 0, 0.2, getRandomColor(null));
-		balls[1] = new Ball(3.5, 2, -2, 0, 0.2, getRandomColor(null));
-
-	}
-
+	
+	// Returns a random color from the neonColors array that is different from the current color
 	private Color getRandomColor(Color currentColor){
 		Color newColor;
 		do {
@@ -56,38 +49,92 @@ class Model {
 		return newColor;
 	}
 
+	Model(double width, double height) {
+		areaWidth = width;
+		areaHeight = height;
+		
+		// Initialize the model with a few balls
+		balls = new Ball[2];
+
+		balls[0] = new Ball(2.5, 2,  1, 0, 0.4, getRandomColor(null));
+		balls[1] = new Ball(3.5, 2, -0.5, 0, 0.2, getRandomColor(null));
+
+	}
+
+	// Add this flag to your Model class
+	boolean showBackgroundImage = false;
+	int frameCounter = 0; // To control how many frames to show the image
+
 	void step(double deltaT) {
 
-		// TODO this method implements one step of simulation with a step deltaT
 		for (Ball b : balls) {
-			boolean collisionOccured = false;
 
 			// detect collision between balls
-
+			Boolean collisionOccured = false;
 			for (Ball o : balls) {
 				if (b != o) { // Cannot collide with itself
-					//System.out.println(b);
-					//System.out.println(o);
-					if (o.x + o.radius > b.x - b.radius && o.x - o.radius < b.x + b.radius && o.vx/b.vx < 0) { // Collision with other ball
-						if(o.x + o.radius >= b.x - b.radius){
-							b.x = o.x + o.radius + b.radius;
-						}
-						else{
+					if (o.x + o.radius > b.x - b.radius && o.x - o.radius < b.x + b.radius) { // Collision with other ball
+						System.out.println("Collision");
+						// move balls to each others edge if overlap occur
+						if(b.x<o.x){
 							b.x = o.x - o.radius - b.radius;
 						}
-						o.vx = -o.vx;
-						b.vx = -b.vx;
+						else if(b.x>o.x){
+							b.x = o.x + o.radius + b.radius;
+						}
+						// apply new velocity
+						b.vx = calculateNewVelocity(b, o)[0];
+						o.vx = calculateNewVelocity(b, o)[1];
+						System.out.println(b.vx);
+						System.out.println(o.vx);
+
+						/*
+						if (b.kx-o.kx < 0){
+							//b.vx = -sqrt(abs(2 * (b.kx - o.kx)) / b.mass);
+						}
+						else if (b.kx-o.kx >= 0) {
+							//b.vx = sqrt(abs(2 * (b.kx - o.kx)) / b.mass);
+						}
+						if (o.kx-b.kx < 0){
+							//o.vx = -sqrt(abs(2*(o.kx-b.kx))/o.mass);
+						}
+						else if (o.kx-b.kx >= 0) {
+							//o.vx = sqrt(abs(2*(o.kx-b.kx))/o.mass);
+						}
+
+						System.out.println(b.vx);
+						System.out.println(o.vx);
+						System.out.println(o.vx);
+						double tempEtot = (b.mass *pow(b.vx,2)+o.mass *pow(o.vx,2))/2;
+
+
+						double bm = b.mx;
+						double om = o.mx;
+						b.mx = om;
+						o.mx = bm;
+						o.kx = 0.5 * o.mass * Math.pow(o.vx, 2); // Kinetic energy along x-axis
+						b.kx = 0.5 * b.mass * Math.pow(b.vx, 2); // Kinetic energy along x-axis
+						System.out.println(tempEtot == (b.mass *pow(b.vx,2)+o.mass *pow(o.vx,2))/2);
+						*/
+
 						collisionOccured = true;
-						System.out.println("Collision");
+						showBackgroundImage = true;
+						frameCounter = 2;
 					}
 
 				}
 				// Do something
+				if (frameCounter > 0) {
+					frameCounter--;
+				} else {
+					showBackgroundImage = false; // Reset the flag after specified frames
+				}
 			}
 
 			// detect collision with the border
 			if (b.x < b.radius || b.x > areaWidth - b.radius) {
 				b.vx *= -1; // change direction of ball
+				b.mx *= -1;
 				collisionOccured = true;
 			}
 
@@ -99,9 +146,11 @@ class Model {
 					b.y = areaHeight - b.radius;
 				}
 					b.vy *= -1;
+					b.my *= -1;
 					collisionOccured = true;
 			};
-
+			
+			// change color of the ball if collision occurred
 			if (collisionOccured){
 				b.color = getRandomColor(null);
 			}
@@ -113,11 +162,22 @@ class Model {
 		}
 	}
 
+	double[] calculateNewVelocity (Ball b1, Ball b2) {
+		double newVelocityB1;
+		double newVelocityB2;
+		newVelocityB1 = (b1.mass*b2.vx + b2.mass*b2.vx + b2.mass*b1.vx - b1.mass*b1.vx)/(2*b2.mass);
+		newVelocityB2 = b2.vx-newVelocityB1+b1.vx;
+		//vxNewB = -(o.mass * o.vx - b.mass * b.vx - b.mass * o.vx + b.mass * b.vx)/b.mass;
+		//vxNewO = o.vx-(vxNewB - b.vx);
+		double[] newList = {newVelocityB1, newVelocityB2};
+		return newList;
+	}
+
 	/**
 	 * Simple inner class describing balls.
 	 */
 	class Ball {
-		double density = 2;
+		double density = 10;
 		Color color;
 		Ball(double x, double y, double vx, double vy, double r, Color color) {
 			this.x = x;
@@ -126,12 +186,16 @@ class Model {
 			this.vy = vy;
 			this.radius = r;
 			this.color = color;
-			this.mass = density* Math.pow(PI,2);
+			this.mass = density * PI * Math.pow(r, 2);
+			this.mx = mass * vx; // Momentum along x-axis
+			this.my = mass * vy; // Momentum along y-axis
+			this.kx = 0.5 * mass * Math.pow(vx, 2); // Kinetic energy along x-axis
+			this.ky = 0.5 * mass * Math.pow(vy, 2); // Kinetic energy along y-axis
 		}
 
 		/**
 		 * Position, speed, radius and mass of the ball.
 		 */
-		double x, y, vx, vy, radius, mass;
+		double x, y, vx, vy, radius, mass, mx, my, kx, ky;
 	}
 }
